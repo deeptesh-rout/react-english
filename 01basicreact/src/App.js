368bafec1f93bce0c8389867677391d8c2f85049
deeptesh-rout <153610471+deeptesh-rout@users.noreply.github.com>
2024-09-22 09:43:22 +0530

    first commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <>
+    <h1>classic react app</h1>
+    <Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import Youtube from "./Youtube"
+
+function App() {
+  const username = "hitesh"
+  return (
+    <>
+      <h1>Vite react {" "} app {2+2}</h1>
+      <h1>Vite react app {} </h1>
+      <Youtube />
+    </>
+  )
+}
+
+export default App
